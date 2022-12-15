152774906ee8e8f73b3f0830bd3163c0bf65ea81
Qiang Yu <qiangyu@gmail.com>
2022-12-14 23:14:18 -0800

    test cgroup mkdir

--- raw_tracepoint_cgroup_mkdir.py
+++ raw_tracepoint_cgroup_mkdir.py
@@ -4,10 +4,17 @@ from ctypes import *
 
 bpf_text = ""
 bpf_program = """
-#include <linux/fs.h>
-#include <linux/sched.h>
+#include <linux/cgroup.h>
+
 
 RAW_TRACEPOINT_PROBE(cgroup_mkdir) {
+    // https://github.com/torvalds/linux/blob/master/include/trace/events/cgroup.h#L54
+    
+    struct cgroup *cgrp = (struct cgroup *)ctx->args[0];
+    const char *path = (const char *)ctx->args[1];
+
+    bpf_trace_printk("path-->%s level %d", path, cgrp->level);
+
 	return 0;
 }
 """
