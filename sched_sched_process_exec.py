cfd880b0e707d1c9779d14c13eacfacb817ae8a6
Qiang Yu <qiangyu@gmail.com>
2022-12-22 17:41:06 -0800

    test reading namespace ids

--- sched_sched_process_exec.py
+++ sched_sched_process_exec.py
@@ -5,8 +5,14 @@ from ctypes import *
 bpf_text = ""
 bpf_program = """
 
+#include <linux/cred.h>
 #include <linux/types.h>
 #include <linux/sched.h>
+#include <linux/mm_types.h>
+#include <linux/nsproxy.h>
+#include <linux/user_namespace.h>
+#include <net/net_namespace.h>
+
 
 struct sched_process_exec_args {
 	unsigned short common_type;
@@ -23,8 +29,28 @@ TRACEPOINT_PROBE(sched, sched_process_exec) {
 	
     // https://github.com/iovisor/bpftrace/issues/999
     // https://github.com/iovisor/bpftrace/issues/385
-    struct sched_process_exec_args *spea = (struct sched_process_exec_args *)args;
+    
+	// Print binary name.
+	struct sched_process_exec_args *spea = (struct sched_process_exec_args *)args;
     bpf_trace_printk("filename--->%s  %d",  ((u64)args + (u64)(spea->filename & 0xFFFF)), spea->pid);
+
+	// Show args.
+	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+	struct mm_struct *mm;
+	bpf_probe_read(&mm, sizeof(mm), &task->mm);
+	
+	unsigned long arg_start, arg_end;
+	bpf_probe_read(&arg_start, sizeof(arg_start), &mm->arg_start);
+	bpf_probe_read(&arg_end, sizeof(arg_end), &mm->arg_end);
+
+	bpf_trace_printk("first args (filename)->%s", (char *)arg_start);
+
+	// Check the namespaces:
+	// readlink /proc/<PID>/ns/net
+	// Net namespace.
+	bpf_trace_printk("net namespace id: %u", task->nsproxy->net_ns->ns.inum);
+	// User namespace is in a different place.
+	bpf_trace_printk("user namespace id: %u", task->cred->user_ns->ns.inum);
     return 0;
 }
 """
